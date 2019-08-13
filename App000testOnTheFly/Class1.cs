573cb23462070a079b49fcf40f8ec27a7ae4a364
agylden <admin@TA-8215-04>
2019-08-13 11:05:55 +0200

    New upload

--- App000testOnTheFly/Class1.cs
+++ App000testOnTheFly/Class1.cs
@@ -6,5 +6,25 @@ namespace App000testOnTheFly
 {
     class Class1
     {
+        static void Main(string[] args)
+        {
+            // Stopur
+            System.Diagnostics.Stopwatch s = new System.Diagnostics.Stopwatch();
+            s.Start();
+            string a = "";
+            for (int i = 0; i < 50000; i++)
+            {
+                a += "*";
+            }
+            s.Stop();
+            Console.WriteLine($"ms = {s.ElapsedMilliseconds}");
+
+            /*
+             ---------- Output: ----------
+
+            ms = 1427
+
+            */
+        }
     }
 }
---
+++ App03Opgave45-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App03Opgave45_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
